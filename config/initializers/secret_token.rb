d8f3d9d8ae2e80f71be709bc5f2e5c6706267783
datsuns <the.skeleton7@gmail.com>
2013-08-19 12:32:22 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jinroh::Application.config.secret_token = '7283ab486c1af11d305427e46076be5b89c1b51de75c4e4abd8e2b8106b5c567702ede7eead554821de0b7fa6ae4be7aeb26973998add30e703b38304ef22e55'
