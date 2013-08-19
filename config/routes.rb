4d8639d3ac8e0b52fab2cfcbcf3393addb8e5d21
datsuns <the.skeleton7@gmail.com>
2013-08-19 23:49:36 +0900

    fix route of root to game#new

--- config/routes.rb
+++ config/routes.rb
@@ -53,7 +53,7 @@ Jinroh::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'game#new', as: 'new'
 
   # See how all your routes lay out with "rake routes"
 
